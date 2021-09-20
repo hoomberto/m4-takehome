3d24900a6540c60ddff23918f61d670f1c73dd68
Bobby Vasquez <shootbv@gmail.com>
2021-09-20 10:34:04 -0600

    Add test to log data upon successful fetch and initial render

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import {getStoriesByType} from '../../utilities/apiCalls.js'
 
 const App = () => {
+
+  useEffect(() => {
+    getStoriesByType('world')
+    .then(data => console.log(data))
+  }, [])
+
   return (
     <div>
-
+      <h1>NewsReader</h1>
     </div>
   )
 }
