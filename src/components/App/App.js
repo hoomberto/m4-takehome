4438d89899d6cd4d44fb82e4e8aa9addf09b8873
Bobby Vasquez <shootbv@gmail.com>
2021-09-21 23:28:33 -0600

    Add context provider, Routes for homepage and individual articles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-// import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 // import {getStoriesByType} from '../../utilities/apiCalls.js'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import TopArticles from '../TopArticles/TopArticles.js'
 import Header from '../Header/Header.js'
+import ArticleContext from '../ArticleContext/ArticleContext.js'
+import DetailedArticle from '../DetailedArticle/DetailedArticle.js'
 
 const App = () => {
 
+  const [currentArticle, setArticle] = useState(false)
   // useEffect(() => {
   //   getStoriesByType('world')
   //   .then(data => console.log(data))
@@ -14,7 +18,21 @@ const App = () => {
   return (
     <div>
       <Header />
-      <TopArticles />
+      <ArticleContext.Provider value={currentArticle}>
+        <Switch>
+          <Route exact path="/">
+            <TopArticles setArticle={setArticle} />
+          </Route>
+          <Route exact path="/:article" render={({ match }) => {
+              const { article } = match.params
+              return (
+                <div>
+                  {!article ? <Redirect to="/" /> : <DetailedArticle />}
+                </div>
+              )
+            }} />
+        </Switch>
+      </ArticleContext.Provider>
     </div>
   )
 }
