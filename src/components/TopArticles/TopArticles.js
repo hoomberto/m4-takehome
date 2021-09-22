05dd427da655b022a260d78fd9deb7742f9d797c
Bobby Vasquez <shootbv@gmail.com>
2021-09-21 22:21:41 -0600

    Add ArticleOptions component to articles container

--- src/components/TopArticles/TopArticles.js
+++ src/components/TopArticles/TopArticles.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {getStoriesByType} from '../../utilities/apiCalls.js'
 import Article from '../Article/Article'
+import ArticleOptions from '../ArticleOptions/ArticleOptions'
 import './TopArticles.css'
 
 const TopArticles = () => {
@@ -8,22 +9,29 @@ const TopArticles = () => {
 
   useEffect(() => {
     getStoriesByType('world')
-    .then(data => setTopArticlesData(data.results))
+    .then(data => setTopArticlesData(data.results.slice(0,10)))
   }, [])
 
   const renderArticles = () => {
     if (topArticlesData) return topArticlesData.map((article, index) => {
-      return <Article
-              key={index}
-              title={article.title}
-              abstract={article.abstract}
-              media={article.multimedia[0]} />
+      return (
+        <Article
+          key={index}
+          title={article.title}
+          abstract={article.abstract}
+          media={article.multimedia[0]}
+        />
+      )
     })
   }
 
   return (
     <section className="articles-container">
-      {!!topArticlesData && renderArticles()}
+      <div>
+        <h1>Top Articles</h1>
+        <ArticleOptions />
+      </div>
+      {!topArticlesData ? <h2>Loading...</h2> : renderArticles()}
     </section>
   )
 }
