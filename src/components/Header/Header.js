ea95df3ab9a2a37e98eaeb9a14128d4e0e001e6c
Bobby Vasquez <shootbv@gmail.com>
2021-09-22 13:13:18 -0600

    Add link to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import './Header.css'
+import { Link } from 'react-router-dom'
 const dayjs = require('dayjs')
 const LocalizedFormat = require('dayjs/plugin/localizedFormat')
 dayjs.extend(LocalizedFormat)
@@ -6,7 +7,7 @@ dayjs.extend(LocalizedFormat)
 const Header = () => {
   return (
     <header>
-      <h1>NYT NewsBlurbs</h1>
+      <Link to="/"><h1>NYT NewsBlurbs</h1></Link>
       <h2>{dayjs().format('LL')}</h2>
     </header>
   )
