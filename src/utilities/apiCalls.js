4ff116a57f4597835e641332315c47343c1fffc9
Bobby Vasquez <shootbv@gmail.com>
2021-09-22 12:55:53 -0600

    Remove unused variable

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -7,8 +7,6 @@ const checkResponse = (response) => {
   }
 };
 
-const key = process.env.REACT_APP_API_KEY
-
 const getStoriesByType = (type) => {
   const topStories = `https://api.nytimes.com/svc/topstories/v2/${type}.json?api-key=Z5V84Pf5YR3szYdJshjTNO27O0tdcJDv`
   return fetch(topStories)
