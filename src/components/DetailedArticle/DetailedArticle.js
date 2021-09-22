d8b0b0114778a6e4ecbbedfdc32c71a4fc081247
Bobby Vasquez <shootbv@gmail.com>
2021-09-21 23:48:40 -0600

    Add component for single article page

---
+++ src/components/DetailedArticle/DetailedArticle.js
@@ -0,0 +1,23 @@
+import React, {useContext} from 'react'
+import { useHistory, Redirect } from "react-router-dom";
+import ArticleContext from '../ArticleContext/ArticleContext'
+import './DetailedArticle.css'
+
+const DetailedArticle = () => {
+  let history = useHistory();
+  const value = useContext(ArticleContext)
+
+  return (
+    <section>
+    {!value ? <Redirect to="/" /> : <div>
+    <button onClick={() => history.goBack()}>Go Back</button>
+      <img src={value.media.url} alt={value.title} />
+      <h2>{value.title}</h2>
+      <h3>{value.byline}</h3>
+      <p>{value.abstract}</p>
+      </div>}
+    </section>
+  )
+}
+
+export default DetailedArticle
