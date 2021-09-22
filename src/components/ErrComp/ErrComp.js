c0eb8ad9caad31d630996e897ac7dc497776018f
Bobby Vasquez <shootbv@gmail.com>
2021-09-22 12:11:43 -0600

    Add return for all other cases

--- src/components/ErrComp/ErrComp.js
+++ src/components/ErrComp/ErrComp.js
@@ -1,8 +1,17 @@
+import React, { useEffect } from 'react'
+import { Redirect, useHistory } from 'react-router-dom'
 const ErrComp = ({err}) => {
+  let history = useHistory();
 
+  useEffect(() => {
+    setTimeout(() => {
+      history.replace('/');
+    }     , 3000)
+  }, [])
   const renderError = () => {
     if (err === 429) return 'Too many requests! Please wait a little before making more.'
     if (err === 500) return 'Server is currently down, please try later!'
+    return 'Oops! Something went wrong! Redirecting now...'
   }
 
   return renderError()
