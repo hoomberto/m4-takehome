34c2669efee2f81a07d924ecb7e218b4ff12a03b
Bobby Vasquez <shootbv@gmail.com>
2021-09-22 12:56:02 -0600

    Remove unused function

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -1,9 +1,3 @@
-const formatAbstract = (abstract) => {
-  let formatted = abstract;
-  if (abstract.length > 90) formatted = abstract.substring(0, 90) + '...'
-  return formatted
-}
-
 export const formatLink = (title) => {
   return title
   .replaceAll(' ', '-')
