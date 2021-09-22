ea0be748ccec13bc3e728b9c7bb8dfd718dd54a5
Bobby Vasquez <shootbv@gmail.com>
2021-09-21 22:38:03 -0600

    Create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>NewsReader</h1>
+      <div className="b1"></div>
+      <div className="b2"></div>
+      <div className="b3"></div>
+    </div>
+  )
+}
+
+export default Header
