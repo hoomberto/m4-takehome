e90bee8504606fceff0ff240571408374a7c73d8
Bobby Vasquez <shootbv@gmail.com>
2021-09-22 12:53:51 -0600

    Render message for 404 errors

--- src/components/ErrComp/ErrComp.js
+++ src/components/ErrComp/ErrComp.js
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react'
-import { Redirect, useHistory } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 const ErrComp = ({err}) => {
   let history = useHistory();
 
   useEffect(() => {
     setTimeout(() => {
       history.replace('/');
-    }     , 3000)
-  }, [])
+    }, 2500)
+  }, [history])
+
   const renderError = () => {
     if (err === 429) return 'Too many requests! Please wait a little before making more.'
     if (err === 500) return 'Server is currently down, please try later!'
+    if (err === 404) return 'Invalid URL! Redirecting now...'
     return 'Oops! Something went wrong! Redirecting now...'
   }
 
