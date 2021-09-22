d19d83e625926d102ad15c0e09d6774bfe4165f7
Bobby Vasquez <shootbv@gmail.com>
2021-09-21 23:48:04 -0600

    Add link formatting function frmo Article

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -3,3 +3,13 @@ const formatAbstract = (abstract) => {
   if (abstract.length > 90) formatted = abstract.substring(0, 90) + '...'
   return formatted
 }
+
+export const formatLink = (title) => {
+  return title
+  .replaceAll(' ', '-')
+  .replaceAll("/’/g", '')
+  .replaceAll(',', '')
+  .replaceAll('.', '')
+  .replaceAll('’', '')
+  .replaceAll('‘', '')
+}
