801397df4624a270a6940f71965c731dde3d51e9
Bobby Vasquez <shootbv@gmail.com>
2021-09-22 00:44:14 -0600

    Wrap heading element in header tag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,9 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>
+    <header>
       <h1>NewsReader</h1>
-      <div className="b1"></div>
-      <div className="b2"></div>
-      <div className="b3"></div>
-    </div>
+    </header>
   )
 }
 
