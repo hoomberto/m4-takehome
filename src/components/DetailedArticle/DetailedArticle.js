3782fd7889448116482d003528b40ebdcdd288b5
Bobby Vasquez <shootbv@gmail.com>
2021-09-22 12:53:42 -0600

    Add publish date and link to full article

--- src/components/DetailedArticle/DetailedArticle.js
+++ src/components/DetailedArticle/DetailedArticle.js
@@ -2,6 +2,9 @@ import React, {useContext} from 'react'
 import { useHistory, Redirect } from "react-router-dom";
 import ArticleContext from '../ArticleContext/ArticleContext'
 import './DetailedArticle.css'
+const dayjs = require('dayjs')
+const LocalizedFormat = require('dayjs/plugin/localizedFormat')
+dayjs.extend(LocalizedFormat)
 
 const DetailedArticle = ({ id }) => {
   let history = useHistory();
@@ -13,8 +16,10 @@ const DetailedArticle = ({ id }) => {
     <button onClick={() => history.goBack()}>Go Back</button>
       <img src={value.media.url} alt={value.title} />
       <h2>{value.title}</h2>
+      <h3>Published {dayjs(value.date).format('LL')}</h3>
       <h3>{value.byline}</h3>
       <p>{value.abstract}</p>
+      <button><a href={value.url}>View Full Article</a></button>
       </div>
     }
     </section>
